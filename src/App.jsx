9608e4ac10ab0a44fe9d7b22679e294adbc0b3f9
Cristhian Almeida <3correntes@gmail.com>
2024-03-03 03:54:15 -0400

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import './App.css';
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import Login from './components/Login/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import './App.css';
 
 const App = () => {
   return (
